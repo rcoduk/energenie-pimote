47342210919d2b837c3fffeeaa16679699fc781f
leepenney <leepenney@users.noreply.github.com>
2015-02-08 19:17:29 +0000

    First commit

---
+++ energenie.py
@@ -0,0 +1,55 @@
+#! /usr/bin/env python
+
+# import required modules
+import RPi.GPIO as gpio
+import time, sys
+gpio_pins = [13,16,15,11] # ordered to match the order used in the send codes
+transmit_pin = 22
+modulator_pin = 18
+on_codes = ['1011','1111','1110']
+off_codes = ['0011','0111','0110']
+
+def send_code(code_sent):
+	# turn on the modulator to send the code set
+	print('Sending code {} ...'.format(code_sent))
+	time.sleep(0.1) # wait for pins to 'settle'
+	gpio.output(transmit_pin, True) # turn modulator on
+	time.sleep(0.25) # give time for signal to be transmitted
+	gpio.output(transmit_pin, False) # turn modulator off
+	print('Code sent')
+
+def prepare_pins():
+	# select the GPIO pins used for the encoder inputs and set them to 0000
+	output_pins = list(gpio_pins)
+	output_pins.extend([modulator_pin,transmit_pin])
+	for pin in output_pins:
+		gpio.setup(pin, gpio.OUT)
+		gpio.output(pin, False)
+
+def main():
+
+	# get the arguments supplied
+	socket = str(sys.argv[1]) # set to string to handle command of zero, which equates to false
+	action = sys.argv[2]
+
+	if (socket and socket in ['0','1','2']) and (action and action in ['on','off']):
+		# if arguments look okay, continue
+		gpio.setmode(gpio.BOARD)
+		prepare_pins()
+
+		codes = on_codes if action == 'on' else off_codes
+
+		for i,pin_setting in enumerate(codes[int(socket)]):
+			gpio.output(gpio_pins[i], int(pin_setting))
+
+		send_code(codes[int(socket)])
+
+		# clean up pins for next time
+		gpio.cleanup()
+	else :
+		print('There\'s a problem with your arguments\n'
+			'USAGE: energenie.py <socket number> <on/off>\n\nNote: You can use (0 for all sockets)')
+
+if __name__ == "__main__":
+	main()
+
---
+++ readme.md
@@ -0,0 +1,32 @@
+# Energenie Pi-mote Command Line Tool
+
+A python script to allow you to control Energenie sockets using a Raspberry Pi with Energenie's Pi-mote attached.
+
+## Usage
+
+To use the script from the command line, enter commands in the format:
+
+```sh
+./energenie.py <socket num> <on/off>
+```
+
+Note: you will need root privileges to access the GPIO ports
+
+So, to turn on socket one:
+
+```sh
+sudo ./energenie.py 1 on
+```
+
+To turn off all sockets:
+
+```sh
+sudo ./energenie.py 0 off
+```
+
+(currently the script only supports up to two sockets, or two socket codes, to be precise)
+
+## Other Resources
+
+[MiniGirlGeek's demo](https://github.com/MiniGirlGeek/energenie-demo)
+[Energenie's example script](https://energenie4u.co.uk/res/pdfs/ENER314%20UM.pdf)
